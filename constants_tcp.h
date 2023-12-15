254ff19d490ca5bad290530f42c3a2a695954d61
goncalof148 <ferreira.goncalo14@gmail.com>
2023-12-15 19:24:59 +0000

    ghjkl

---
+++ constants_tcp.h
@@ -0,0 +1,33 @@
+#define FNAME 24
+#define FSIZE 8
+#define CODE_SIZE 4
+#define MAX_CMD_SIZE 11
+#define UID_SIZE 6
+#define PASSWORD_SIZE 8
+
+#define CASE_LOGIN 0
+#define CASE_LOGOUT 1
+#define CASE_UNREGISTER 2
+#define CASE_MYAUCTIONS 3
+#define CASE_MYBIDS 4
+#define CASE_SHOW_RECORD 5
+#define CASE_LIST 6
+
+typedef struct{
+    char name[15];
+    char asset_fname[FNAME];
+    int start_value;
+    int timeactive;
+    size_t size[FSIZE];
+    char data[15];
+    int aid;
+    int higher_value;
+
+} Auction;
+
+typedef struct User{
+    char uid[UID_SIZE];
+    char password[PASSWORD_SIZE];
+    int logged_in;
+    Auction auction[1000];
+}User;
